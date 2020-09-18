25e4c2ae25e7cbaf05b5ec5badb279c251d6ba00
David R. Walton <davidwalton3142@gmail.com>
2020-09-18 12:56:59 +0100

    Added gitignore, fixed some issues

--- main/metamerTest.m
+++ main/metamerTest.m
@@ -6,23 +6,23 @@
 % that the code is working. The resulting synthetic image should reproduce the
 % texture of the original within coarse square tiles.
 
-% load original image
-oim = double(imread('example-im-512x512.png'));
-
-% set options
-opts = metamerOpts(oim,'windowType=square','nSquares=[3 1]');
-
-% make windows
-m = mkImMasks(opts);
-
-% plot windows
-plotWindows(m,opts);
-
-% do metamer analysis on original (measure statistics)
-params = metamerAnalysis(oim,m,opts);
-
-% do metamer synthesis (generate new image matched for statistics)
-res = metamerSynthesis(params,size(oim),m,opts);
+% % load original image
+% %oim = double(imread('portico.jpg'));
+% 
+% % set options
+% %opts = metamerOpts(oim,'windowType=square','nSquares=[3 1]');
+% 
+% % make windows
+% m = mkImMasks(opts);
+% 
+% % plot windows
+% plotWindows(m,opts);
+% 
+% % do metamer analysis on original (measure statistics)
+% params = metamerAnalysis(oim,m,opts);
+% 
+% % do metamer synthesis (generate new image matched for statistics)
+% res = metamerSynthesis(params,size(oim),m,opts);
 
 
 %% METAMER DEMO (will take a few min per iteration)
@@ -32,7 +32,8 @@ res = metamerSynthesis(params,size(oim),m,opts);
 % used to generate metamers in Freeman & Simoncelli
 
 % load original image
-oim = double(imread('example-im-512x512.png'));
+%oim = double(imread('example-im-512x512.png'));
+oim = double(imread('portico.jpg'));
 
 % set options
 opts = metamerOpts(oim,'windowType=radial','scale=0.5','aspect=2');
