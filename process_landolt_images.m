36271dacea356adccd453870d368e333f2f68388
David R Walton <davidwalton3142@gmail.com>
2021-01-17 20:41:01 +0000

    Update process_landolt_images.m

--- process_landolt_images.m
+++ process_landolt_images.m
@@ -1,9 +1,11 @@
 % Process Landolt images
 
-for type = ["o", "c"]
-    for angle = 5:30
+
+for angle = 10:30
+    for type = ["c", "o"]
         filename = sprintf("landolt_%s_M_%d.png", type, angle);
         oim = double(imread(filename));
+        oim = oim - rand(size(oim))*0.01;
         output_filename = sprintf("landolt_%s_M_%d_metamer.png", type, angle);
         fprintf("Processing image %s\n", filename);
         fprintf("Producing output %s\n", output_filename);
